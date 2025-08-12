7c32ec661a9da0dde318a8f4226555c990520f46
Zach Caudle <z.caudle96@gmail.com>
2025-08-12 10:21:48 -0400

    feat: Added 2 security policies, one for preventing any actions from the OU root accounts, the other for restricting certain actions outside of the designated region.

--- main.tf
+++ main.tf
@@ -405,3 +405,98 @@ resource "aws_organizations_account" "sandbox" {
 	role_name = "OrganizationAccountAccessRole"
  }
 
+# SCP: Deny Root Account Access
+# This policy prevents the root user of any account its attached to 
+# from performing any actions, forcing the use of IAM users/roles
+resource "aws_organizations_policy" "deny_root_access" {
+	name = "DenyRootAccess"
+	description = "Denies all actions by the root user" 
+	type = "SERVICE_CONTROL_POLICY"
+	content = jsonencode({
+	   Version = "2012-10-17",
+	   Statement = [
+		{
+		  Sid = "DenyRoot",
+		  Effect = "Deny",
+		  Action = "*",
+		  Resource = "*",
+		  Condition = {
+		  "StringNotLike": {
+		      "aws:PrincipalArn": [
+				"arn:aws:iam::*:role/OrganizationAccountAccessRole", # Allow OrganizationAccountAccessRole
+				"arn:aws:iam::*:role/AWSControlTowerExectuion", # If using ControlTower
+				"arn:aws:iam::*:role/AWSReservedSSO_*",
+				"arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+				]
+			    }
+			}
+		    }
+		]
+	    })
+	}
+
+#SCP: Restrict AWS Regions 
+# This policy restricts all accounts to only allow actions in specified reegions
+resource "aws_organizations_policy" "restrict_regions" {
+	name = "RestrictRegion"
+	description = "Allows actions only in specified AWS Regions"
+	type = "SERVICE_CONTROL_POLICY"
+	content = jsonencode({
+	  Version = "2012-10-17",
+	  Statement = [
+		{
+		  Sid = "DenyAllOutsideAllowedRegions",
+		  Effect = "Deny",
+		  NotAction = [ # Actions that allowed globally, even outside the specified region
+			"a4b:*",
+			"aws-portal:*",
+			"budgets:*",
+			"ce:*",
+			"chime:*",
+			"cloudfront:*",
+			"config:DeleteDeliveryChannel",
+			"config:DeleteEvaluationResults",
+			"config:DeleteRetentionConfiguration",
+			"config:DeliverConfigSnapshot",
+			"config:Describe*",
+			"config:Get*",
+			"config:List*",
+			"config:Put*",
+			"config:Select*",
+			"config:StopConfigurationRecorder",
+			"directconnect:*",
+			"ec2:DescribeRegions",
+			"ec2:DescribeAvailabilityZones",
+			"globalaccelerator:*",
+			"iam:*",
+			"importexport:*",
+			"kms:DescribeKey",
+			"organizations:*",
+			"route53:*",
+			"s3:GetAccountPublicAccessBlock",
+			"s3:GetBucketLocation",
+			"s3:GetAccountPublicAccessBlock",
+			"s3:GetBucketPolicy",
+			"s3:ListAllMyBucket",
+			"s3:PutAccountPublicAccessBlock",
+			"s3:PutBucketPolicy",
+			"sso:*",
+			"support:*",
+			"sts:*",
+			"waf:*",
+			"waf-regional:*",
+			"wafv2:*"
+		],
+			Resource = "*",
+			Condition = { 
+			  "StringNotEquals": {
+			      "aws:RequestedRegion": [
+					"us-east-1"
+				]
+			    }
+			}
+		    }
+		]
+	    })
+	}
+
