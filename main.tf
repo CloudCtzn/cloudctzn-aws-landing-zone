2dcb63c7f29dc396bd385f4cfd5236d7e26eee5c
Zach Caudle <z.caudle96@gmail.com>
2025-08-11 22:04:06 -0400

    feat: Finished provisiong the accounts for the different OUs completing the backbone of the infrastructure.

--- main.tf
+++ main.tf
@@ -344,3 +344,64 @@ resource "null_resource" "start_config_recorder" {
     aws_config_config_rule.ec2_instance_no_public_ip
   ]
 }
+
+# Data source to get the ARN of the SSO instance 
+# This is needed to reference the enabled SSO instance in other resources.
+data "aws_ssoadmin_instances" "main" { 
+  }
+
+# Resource to create the Permission Set
+# This defines the a set of permissions that can be assigned to users/groups in accounts.
+# For now, I am creating an AdministrativeAccess Permission Set
+resource "aws_ssoadmin_permission_set" "administrator_access" {
+	name = "AWSAdministratorAccess"
+	instance_arn = tolist(data.aws_ssoadmin_instances.main.arns)[0] # References the SSO instance arn
+	description = "Provides full access to AWS services and resources"
+	session_duration = "PT8H"
+
+	depends_on = [
+		data.aws_ssoadmin_instances.main
+	]
+  }
+
+# Resource to attach a managed policy to the permission set
+resource "aws_ssoadmin_managed_policy_attachment" "administrator_access_attachment" {
+	instance_arn = tolist(data.aws_ssoadmin_instances.main.arns)[0]
+	permission_set_arn = aws_ssoadmin_permission_set.administrator_access.arn
+	managed_policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+
+
+
+	depends_on = [
+	    aws_ssoadmin_permission_set.administrator_access
+	]
+ }
+
+resource "aws_organizations_account" "audit" {
+	name = "Audit"
+	email = "z.caudle96+audit@gmail.com"
+	parent_id = aws_organizations_organizational_unit.security_ou.id
+	role_name = "OrganizationAccountAccessRole"
+ }
+
+resource "aws_organizations_account" "shared_services" {
+	name = "Shared Services"
+	email = "z.caudle96+sharedservices@gmail.com"
+	parent_id = aws_organizations_organizational_unit.infrastructure_ou.id
+	role_name = "OrganizationAccountAccessRole"
+ }
+
+resource "aws_organizations_account" "workload" {
+	name = "Workload"
+	email = "z.caudle96+workload@gmail.com"
+	parent_id = aws_organizations_organizational_unit.workloads_ou.id
+	role_name = "OrganizationAccountAccessRole"
+ }
+
+resource "aws_organizations_account" "sandbox" {
+	name = "Sandbox"
+	email = "z.caudle96+sandbox@gmail.com"
+	parent_id = aws_organizations_organizational_unit.sandbox_ou.id
+	role_name = "OrganizationAccountAccessRole"
+ }
+
